311a12438865bc44bf5a014f8f42005bcd163d9c
hanminthant56 <hanminthant56@gmail.com>
2023-07-14 10:00:49 +0900

    title icon and css changes

--- pages/SimilarWordPage.js
+++ pages/SimilarWordPage.js
@@ -9,8 +9,8 @@ for (let i = 0; i < similarword.length; i++) {
     //   [{"word":"取ります (自)", "meaning":"Take", "Eg":["年を取ります"]}],
     //]
     similarworddata += `
-    <div class="alert alert-dark" role="alert">
-        `+ list[0].word +`
+    <div class="alert alert-dark mb-3" role="alert">
+        `+ list.word +`
         <a tabindex="0" class="btn btn-info" role="button" 
             data-bs-toggle="popover" 
             data-bs-trigger="focus" 
@@ -20,6 +20,13 @@ for (let i = 0; i < similarword.length; i++) {
         </a>
     </div>
     `;
+    switch(list.space) {
+        case "needed":
+            similarworddata += "<div class='m-3'></div>";
+            break;
+        case "little needed":
+            similarworddata += "<div class='m-2'></div>";
+    }
 }
 // End For loop of the whole data
 //preparing by adding all the data by adding to the relating box or pages
